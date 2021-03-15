c3aa1d2b6b8d7516e20aed14d9e0f16758be6bc3
ahgumasing <ahgumasing@gmail.com>
2021-03-15 11:25:18 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="h-screen" :class="theme">
+      <div>
+        <button
+          class="focus:outline-none"
+          :title="
+            theme == 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode'
+          "
+          @click="toggleDarkMode"
+        >
+          <icon v-if="theme == 'dark'" name="moon" class="w-8 h-8"></icon>
+          <icon v-else name="sun" class="w-8 h-8"></icon>
+        </button>
+      </div>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
-    <router-view />
+    <!-- <router-view /> -->
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Icon from "./components/Icon.vue";
+export default {
+  name: "app",
+  components: {
+    Icon
+  },
+  data() {
+    return {
+      theme: "dark"
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      if (this.theme == "dark") {
+        this.theme = this.theme = "light";
+      } else {
+        this.theme = this.theme = "dark";
+      }
     }
   }
+};
+</script>
+
+<style lang="scss">
+.dark {
+  @apply dark:bg-red-600;
 }
 </style>
